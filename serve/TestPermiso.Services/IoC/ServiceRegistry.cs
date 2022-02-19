bb005b2e988b9127347d20833c121fa3c1e78431
JuanD <juand0014@hotmail.com>
2022-02-19 01:45:22 -0400

    configurate ServicesRegistry

---
+++ serve/TestPermiso.Services/IoC/ServiceRegistry.cs
@@ -0,0 +1,17 @@
+﻿using GenericApi.Services.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Services.IoC
+{
+    public static class ServiceRegistry
+    {
+        public static void AddServiceRegistry(this IServiceCollection services)
+        {
+            services.AddScoped<IPermissionsService, PermissionsServices>();
+            services.AddScoped<IPermissionTypeService, PermissionTypeServices>();
+        }
+    }
+}
