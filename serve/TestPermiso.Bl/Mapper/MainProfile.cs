bb953cdf28f18e8518865cd9e97d6fc67545f7a4
JuanD <juand0014@hotmail.com>
2022-02-19 01:20:51 -0400

    Add AutoMapper

---
+++ serve/TestPermiso.Bl/Mapper/MainProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using GenericApi.Bl.Dto;
+using GenericApi.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestPermiso.Model.Entities;
+
+namespace GenericApi.Bl.Mapper
+{
+    public class MainProfile : Profile
+    {
+        public MainProfile()
+        {
+            CreateMap<Permissions, PermissionsDto>().ReverseMap();
+            CreateMap<PermissionType, PermissionTypeDto>().ReverseMap();
+        }
+    }
+}
