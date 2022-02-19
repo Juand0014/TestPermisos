3036a673b7a8bce6f717aafcc5bc9089e01697a6
JuanD <juand0014@hotmail.com>
2022-02-19 00:31:54 -0400

    add base entity and Dtos

---
+++ serve/TestPermiso.Core/BaseModel/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Core.BaseModel
+{
+    public interface IBaseEntity : IBase
+    {
+        DateTime? DeletedDate { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string CreatedBy { get; set; }
+        string DeletedBy { get; set; }
+        string UpdatedBy { get; set; }
+    }
+    public class BaseEntity : Base, IBaseEntity
+    {
+        public virtual DateTime? DeletedDate { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime? UpdatedDate { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual string DeletedBy { get; set; }
+        public virtual string UpdatedBy { get; set; }
+    }
+}
---
+++ serve/TestPermiso.Core/BaseModel/BaseEntityDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Core.BaseModel
+{
+    public interface IBaseEntityDto : IBaseDto
+    {
+        DateTime? DeletedDate { get; set; }
+        DateTime? CreatedDate { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string CreatedBy { get; set; }
+        string DeletedBy { get; set; }
+        string UpdatedBy { get; set; }
+    }
+    public class BaseEntityDto : BaseDto, IBaseEntityDto
+    {
+        public virtual DateTime? DeletedDate { get; set; }
+        public virtual DateTime? CreatedDate { get; set; }
+        public virtual DateTime? UpdatedDate { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual string DeletedBy { get; set; }
+        public virtual string UpdatedBy { get; set; }
+    }
+}
