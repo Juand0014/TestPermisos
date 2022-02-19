be78312cf2f878e31f37b4ef6e1cdcd13cbb2b38
JuanD <juand0014@hotmail.com>
2022-02-19 11:44:51 -0400

    add test to project

---
+++ serve/TestPermiso.Services.Test/Data/ServicesTestsData.cs
@@ -0,0 +1,25 @@
+﻿using GenericApi.Core;
+using GenericApi.Model.Entities;
+using System;
+using TestPermiso.Model.Entities;
+
+namespace Dallas.Services.Tests.Data
+{
+    public class ServicesTestsData
+    {
+        public readonly Permissions _permissions = new Permissions
+        {
+            Name = "Juan",
+            LastName = "Matos",
+            DatePermission = DateTime.Now,
+            
+        };
+
+        public readonly Permissions _permissions2 = new Permissions
+        {
+            Name = "Pedro",
+            LastName = "Martinez",
+            DatePermission = DateTime.Now
+        };
+    }
+}
