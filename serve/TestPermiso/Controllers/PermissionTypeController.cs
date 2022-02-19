1c42420c7966e5639f9c2dc6b448d05fa9fbad55
JuanD <juand0014@hotmail.com>
2022-02-19 02:01:59 -0400

    Add permissionType Controller

---
+++ serve/TestPermiso/Controllers/PermissionTypeController.cs
@@ -0,0 +1,14 @@
+﻿using GenericApi.Bl.Dto;
+using GenericApi.Model.Entities;
+using GenericApi.Services.Services;
+using TestPermiso.Model.Entities;
+
+namespace GenericApi.Controllers
+{
+    public class PermissionTypeController : BaseController<PermissionType, PermissionTypeDto>
+    {
+        public PermissionTypeController(IPermissionTypeService service) : base(service)
+        {
+        }
+    }
+}
