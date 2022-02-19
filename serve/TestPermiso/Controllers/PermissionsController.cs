a45ec62730d6b1b3c9bcef774a4a6192979d1f01
JuanD <juand0014@hotmail.com>
2022-02-19 02:00:51 -0400

    add permissions controllers

---
+++ serve/TestPermiso/Controllers/PermissionsController.cs
@@ -0,0 +1,13 @@
+﻿using GenericApi.Bl.Dto;
+using GenericApi.Model.Entities;
+using GenericApi.Services.Services;
+
+namespace GenericApi.Controllers
+{
+    public class PermissionsController : BaseController<Permissions, PermissionsDto>
+    {
+        public PermissionsController(IPermissionsService service) : base(service)
+        {
+        }
+    }
+}
--- serve/TestPermiso/Startup.cs
+++ serve/TestPermiso/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using GenericApi.Bl.Config;
+using GenericApi.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
