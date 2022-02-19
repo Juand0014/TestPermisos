5a89df073265e9599e85e7b52beb809cb895de76
JuanD <juand0014@hotmail.com>
2022-02-19 01:35:43 -0400

    Add services to entities

---
+++ serve/TestPermiso.Services/Services/PermissionTypeServices.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using FluentValidation;
+using GenericApi.Bl.Dto;
+using GenericApi.Bl.Validations;
+using GenericApi.Model.Entities;
+using GenericApi.Model.Repositories;
+using TestPermiso.Model.Entities;
+
+namespace GenericApi.Services.Services
+{
+    public interface IPermissionTypeService : IBaseService<PermissionType, PermissionTypeDto>
+    {
+
+    }
+    public class PermissionTypeServices : BaseService<PermissionType, PermissionTypeDto>, IPermissionTypeService
+    {
+        public PermissionTypeServices(
+            IPermissionTypeRepository repository,
+            IMapper mapper,
+            IValidator<PermissionTypeDto> validator) : base(repository, mapper, validator)
+        {
+
+        }
+    }
+}
+
---
+++ serve/TestPermiso.Services/Services/PermissionsServices.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FluentValidation;
+using GenericApi.Bl.Dto;
+using GenericApi.Bl.Validations;
+using GenericApi.Model.Entities;
+using GenericApi.Model.Repositories;
+
+namespace GenericApi.Services.Services
+{
+    public interface IPermissionsService : IBaseService<Permissions, PermissionsDto>
+    {
+
+    }
+    public class PermissionsServices : BaseService<Permissions, PermissionsDto>, IPermissionsService
+    {
+        public PermissionsServices(
+            IPermissionsRepository repository,
+            IMapper mapper,
+            IValidator<PermissionsDto> validator) : base(repository, mapper, validator)
+        {
+
+        }
+    }
+}
+
