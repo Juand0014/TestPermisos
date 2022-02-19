2591f0e1d6aa64d1c4d72d4d237d325211195fb9
JuanD <juand0014@hotmail.com>
2022-02-19 01:16:36 -0400

    configurate validation for props and configurate sqlServerDbContext

---
+++ serve/TestPermiso.Bl/Validations/PermissionTypeValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using GenericApi.Bl.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Bl.Validations
+{
+    public class PermissionTypeValidator : AbstractValidator<PermissionTypeDto>
+    {
+		public PermissionTypeValidator()
+		{
+			RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+		}
+	}
+}
---
+++ serve/TestPermiso.Bl/Validations/PermissionsValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using GenericApi.Bl.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericApi.Bl.Validations
+{
+    public class PermissionsValidator : AbstractValidator<PermissionsDto>
+    {
+		public PermissionsValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("User's Name is required");
+			RuleFor(x => x.LastName).NotEmpty().WithMessage("Lastname is required");
+		}
+	}
+}
