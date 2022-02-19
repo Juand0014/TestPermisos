88c2618d581f45349bf9225bedbd511c4eff0039
JuanD <juand0014@hotmail.com>
2022-02-19 00:21:22 -0400

    initial commit

---
+++ TestPermiso/Startup.cs
@@ -0,0 +1,49 @@
+﻿using TestPermiso.Bl.Config;
+using TestPermiso.Config;
+using TestPermiso.Core.Settings;
+using TestPermiso.Model.IoC;
+using TestPermiso.Services.IoC;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TestPermiso
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MainPolicy",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors("MainPolicy");
+        }
+    }
+}
